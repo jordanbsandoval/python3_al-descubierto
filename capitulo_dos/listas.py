80e472ac99bf127e8e3cd97d4708dd71817e49d3
jordanbsandoval <1608@holbertonschool.com>
2021-04-02 18:52:00 -0400

    listas_objetos

---
+++ capitulo_dos/listas.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# es mutable
+# Basicamente una lista es una coleccion ordenada de objetos similar a un 
+# array dinamico: No tiene un tamano fijo, permiten que se quiten o anadan elementos
+
+# definiciendo una lista
+
+lista = []          # defino una lista vacia
+
+lista = [2, 4.5, "hola parce"]      # defino una lista entre corchete 
+lista[1] = 3.5
+
+print(lista)
