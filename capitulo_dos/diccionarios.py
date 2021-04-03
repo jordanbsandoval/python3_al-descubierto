538c67e6aa945a178fa2e66f22e410807760a2c6
jordanbsandoval <1608@holbertonschool.com>
2021-04-03 17:14:52 -0400

    diccionario_modifica

--- capitulo_dos/diccionarios.py
+++ capitulo_dos/diccionarios.py
@@ -5,3 +5,10 @@
 diccionario = {'a' : 1, 'b' : 2, 'c' : 3}
 print("diccionario con elementos: {}".format(diccionario))
 
+diccionario = dict(a=2, b=3, c=3.5)         # tambien puedo declarar un diccionario con la funcion
+                                            # dict
+print("declarando diccionario con la funcion dict(): {}".format(diccionario))
+
+diccionario['a'] = 12
+
+print("modificando el valor de un elemento atraves de la palabra clave: {}".format(diccionario))
