a416cf58e933e8372d1e6addfac50cdaea18d523
jordanbsandoval <1608@holbertonschool.com>
2021-04-03 14:04:05 -0400

    diccionarios

---
+++ capitulo_dos/diccionarios.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# es mutable
+# es una estructura de datos
+# internamente es representado como una tabla hash. que reprsenta rapidez de acceso y flexibilidad
+diccionario = {'a' : 1, 'b' : 2, 'c' : 3}
+print("diccionario con elementos: {}".format(diccionario))
+
