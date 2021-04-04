5cf14dcd6f3237a15972b61e1300897b0eb3edf3
jordanbsandoval <1608@holbertonschool.com>
2021-04-04 14:19:43 -0400

    funciones

---
+++ capitulo_tres/funciones.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# declaracion y llamado a una funcion
+def funcion():              # palabra clave para definir una funcion que sera un objeto
+                            # y el nombre utilizado seguido de la palabra clave def sera quien
+                            # apunte al objeto funcion, posteriormente no tiene parametros
+    print("invocando la funcion")
+funcion()                   # llamando a la funcion
+
+# Paso de parametros a funciones
