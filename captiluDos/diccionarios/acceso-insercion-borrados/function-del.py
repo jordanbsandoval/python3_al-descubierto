d28e9f064fdafeaab91c11c239299628814a74ce
jordanbsandoval <1608@holbertonschool.com>
2021-08-25 17:08:53 -0500

    metodos-items-keys-values-fucitondel

---
+++ captiluDos/diccionarios/acceso-insercion-borrados/function-del.py
@@ -0,0 +1,5 @@
+""" function del(): Elimina un elemento del diccionario atraves de su clave """
+dicc = {'edad': 28, 'nombre':"jordan", 'peso':"67.2"}
+print(dicc)
+del(dicc['peso'])
+print(dicc)
---
+++ captiluDos/diccionarios/acceso-insercion-borrados/metodo-keys-list.py
@@ -0,0 +1,5 @@
+""" Metodo keys: Obtendremos las claves de un diccionario, almacenandolos en una lista """
+dicc = {'edad': 28, 'nombre':"jordan", 'peso':"67.2"}
+print(dicc)
+lista = list(dicc.keys())
+print(lista)
