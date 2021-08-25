5dab8d6214c64256f318d49b34abb59905c7dbaa
jordanbsandoval <1608@holbertonschool.com>
2021-08-25 16:14:54 -0500

    diccionarios

---
+++ captiluDos/diccionarios/declarando-diccionario.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+""" Declarando un diccionario: se utilizan las llaves ({}), entre las que se encuentran los pares, clave:valor. Separados por coma"""
+dicc = {'edad': 28, 'nombre':"jordan", 'peso':"67.2"}
+print(dicc)
---
+++ captiluDos/diccionarios/function-dict.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" function dict: Nos permite declarar un diccionario """
+dicc = {}
+print(dicc)
+dicc = dict(a=1, b=4, c="jordan")
+print(dicc)
