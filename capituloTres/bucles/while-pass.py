94259f47fdd74a900707865a5619ba2e1130d381
jordanbsandoval <1608@holbertonschool.com>
2021-08-26 00:18:54 -0500

    sentencia-pass

---
+++ capituloTres/bucles/while-pass.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" sentencia pass: Se utiliza para dar un esqueletos de codigo que luego rellenaremos 
+                    con codigo funcional """
+a, b = 0, 11
+while a < b:
+    if a % 2 != 0:
+        pass
+    a += 1
